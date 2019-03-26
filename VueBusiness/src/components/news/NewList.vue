ed16c3abd7fe652028c66140359e44f5c0b656e6
liuzhihua <liuzhihua@lushangchuangfu.com>
2019-03-26 18:21:48 +0800

    新闻列表接口走通，并渲染页面

--- VueBusiness/src/components/news/NewList.vue
+++ VueBusiness/src/components/news/NewList.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>新闻列表</div>
+  <div>
+    <ul class="mui-table-view mui-table-view-chevron">
+      <li class="mui-table-view-cell mui-media" v-for="item in newList" :key="item.id">
+        <a class="mui-navigate-right">
+          <img class="mui-media-object mui-pull-left" :src="item.img_url">
+          <div class="mui-media-body">
+            <h6>{{item.title}}</h6> 
+            <h7 class="mui-ellipsis">{{item.zhaiyao}}</h7>
+            <div>
+              <span>添加时间：{{item.add_time}}</span>
+              <span>点击次数{{item.click}}</span>
+            </div>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
@@ -11,10 +27,11 @@ export default {
   created() {
     this.$get(this.urlApi.newListUrl, {})
       .then(res => {
-        console.log(res)
+        // console.log(res)
+        this.newList = res.data;
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
       });
   }
 };

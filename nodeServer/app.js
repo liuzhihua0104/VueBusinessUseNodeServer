588468411c239cfbc3505340a2e21c5de67fc739
liuzhihua <liuzhihua@lushangchuangfu.com>
2019-03-26 17:49:04 +0800

    删除废弃的代码

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -12,51 +12,10 @@ app.all('*', function (req, res, next) {
 });
 
 
-// // 使用mongodb
-// let MongoClient = require("mongodb").MongoClient;
-// let dbUrl = "mongodb://localhost:27017";
-
-// app.get("/api/getlunbo", function (req, res) {
-//   MongoClient.connect(dbUrl, { useNewUrlParser: true }, function (err, db) {
-//     if (err) {
-//       res.json({
-//         code: 200,
-//         msg: "连接数据库失败"
-//       })
-//       return
-//     }
-//     // 连接数据库成功，从数据库查找数据
-//     let targetDb = db.db("vuebuspro"); // 目标数据库
-//     let result = targetDb.collection("home_img").find()  //去home_img集合中查找所有数据
-//     result.toArray(function (err, data) {
-//       if (err) {
-//         res.json({
-//           code: 200,
-//           msg: "读取数据失败"
-//         })
-//         return
-//       }
-//       res.json({
-//         code: 200,
-//         message: data,
-//         msg: "获取图片成功"
-//       })
-//     })
-//     db.close();
-//     console.log("close finish")
-//   })
-
-// })
-
-
-
-
 // 使用mongodb
 let MongoClient = require("mongodb").MongoClient;
 let dbUrl = "mongodb://localhost:27017";
 // let dbUrl="mongodb://liuzhihuarose@126.com:13910830226AAAAA!@ds121996.mlab.com:21996/rosemlabdate";
-
-
 
 // 先连接数据库
 MongoClient.connect(dbUrl, { useNewUrlParser: true }, function (err, db) {
@@ -94,7 +53,6 @@ MongoClient.connect(dbUrl, { useNewUrlParser: true }, function (err, db) {
   // 新闻列表
   app.get("/api/getnewslist", function (req, res) {
     let targetDb = db.db("vuebuspro"); // 目标数据库
-    // let targetDb = db.db("rosemlabdate"); // 目标数据库
     let result = targetDb.collection("newlist").find() //去newlist集合中查找所有数据，如果没有应该会自动创建
     result.toArray(function (err, data) {
       if (err) {
@@ -140,26 +98,8 @@ MongoClient.connect(dbUrl, { useNewUrlParser: true }, function (err, db) {
         })
       }
     })
-
-    return
-
-
-
-
-
-
   })
-
-
-
-
-
 })
 
 
 app.listen(9999, "localhost")
-
-// "192.168.40.192" 为本机的ipv4编码
-// app.listen(9999,"localhost")
-
-// http://192.168.40.192:9999/api/getlunbo

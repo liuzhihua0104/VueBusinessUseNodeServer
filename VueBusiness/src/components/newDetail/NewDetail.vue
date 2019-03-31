5f2c926bb8b67496fc0035993899cfe02668cc06
liuzhihua <liuzhihua@lushangchuangfu.com>
2019-03-31 18:15:38 +0800

    渲染新闻详情页面

--- VueBusiness/src/components/newDetail/NewDetail.vue
+++ VueBusiness/src/components/newDetail/NewDetail.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="container">新闻详情页面{{this.$route.params.id}}</div>
+  <div class="container">
+    <h4>{{newDetail.title}}</h4>
+    <div class="detail-des">
+      <span>{{newDetail.add_time|dateFormat}}</span>
+      <span>点击次数：{{newDetail.click}}</span>
+    </div>
+    <div v-html="newDetail.content"></div>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      html: ""
+      newDetail: {}
     };
   },
   created() {
     console.log(this.$route.params.id);
-    // let that=this;
+    let that = this;
     this.$get("/api/getnew/" + this.$route.params.id)
       .then(res => {
         console.log(res);
+        that.newDetail = res.data[0];
       })
       .catch(err => {
         console.log(err);
@@ -26,6 +34,10 @@ export default {
 .container {
   width: 100%;
   height: 100%;
+  .detail-des{
+    display: flex;
+    justify-content: space-between;
+  }
 }
 </style>
 
